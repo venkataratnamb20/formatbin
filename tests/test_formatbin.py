be7877f4446fa715d353f25516dd6d6d2e52f17b
venkataratnamb20 <venkat4ratnam@gmail.com>
2024-07-07 03:06:12 +0530

    Functional Tests:
    1) tests to import functions
    2) tests to test existance of files and format

---
+++ tests/test_formatbin.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+import shutil
+import unittest
+
+from formatbin import process_binary, create_files
+
+
+class TestFormatBin(unittest.TestCase):
+    def setUp(self):
+        self.n = 8
+        self.filename = "input.txt"
+        self.filename = Path(self.filename)
+        self.results_dir = self.filename.stem
+        
+    def tearDown(self):
+        if os.path.exists(self.results_dir):
+            shutil.rmtree(self.results_dir)
+        
+        
+    def test_create_files(self):
+        data = process_binary(self.filename, self.n)
+        create_files(data, self.results_dir)
+        self.assertTrue(self.results_dir.is_dir())
+        for i in range(n):
+            file_suffix = f'0{i}' if i < 10 else f'{i}'
+            self.assertTrue(
+                Path(self.results_dir, f"input_{file_suffix}.txt").is_file())
+        self.assertTrue(self.results_dir.is_dir())
+        
+    def test_process_binary(self):
+        data = process_binary(self.filename,  self.n)
+        self.assertTrue(False)
+        
+    
+if __name__ == '__main__':
+    unittest.main(argv=[''], verbosity = 2, exit = False)
+        
